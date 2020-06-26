739fe2d808dcbeb290876f2b56d5c1cca4ab7137
Ivan Quintana <ivanquisco@gmail.com>
2020-06-26 15:02:41 -0500

    initial commit, .gitignore added

---
+++ FinanzasFirst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinanzasFirst
+//
+//  Created by TACODE on 26/06/20.
+//  Copyright © 2020 tacode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
